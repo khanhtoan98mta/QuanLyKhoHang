fadac2e22474dd5d49e8b522ee1a79636f7b5195
khanhtoan99gvc <khanhtoan99gvc@gmail.com>
2019-10-01 08:32:42 +0700

    main form

--- QuanLyKhoHang/Form1.Designer.cs
+++ QuanLyKhoHang/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.btn_PhieuNhap = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_PhieuNhap
@@ -50,11 +51,21 @@
             this.button1.Text = "Phiếu xuất";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Quản lý kho";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_PhieuNhap);
             this.Name = "Form1";
@@ -67,6 +78,7 @@
 
         private System.Windows.Forms.Button btn_PhieuNhap;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
