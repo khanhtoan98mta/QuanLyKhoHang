02439d411c49aabe0eccb4ff3da20a2d72159d35
lanhuong1598 <lanhuongitk55lvt@gmail.com>
2019-10-14 15:57:49 +0700

    update form Chitietphieunhap

--- QuanLyKhoHang/GUI/Chitietphieunhap.Designer.cs
+++ QuanLyKhoHang/GUI/Chitietphieunhap.Designer.cs
@@ -193,7 +193,10 @@
             this.dataGridView2.ReadOnly = true;
             this.dataGridView2.Size = new System.Drawing.Size(831, 293);
             this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            this.dataGridView2.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            this.dataGridView2.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // txtTongtien
             // 
