47454cdf4ab038bfc1f84906e3cb70aa236cbd46
lanhuong1598 <lanhuongitk55lvt@gmail.com>
2019-10-14 10:16:56 +0700

    Add  Function Load data form San Pham

--- QuanLyKhoHang/GUI/User_Control/Khachhang.cs
+++ QuanLyKhoHang/GUI/User_Control/Khachhang.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QuanLyKhoHang;
+using System.Data.SqlClient;
 //using QuanlyKhohang.BUS;
 
 namespace QuanlyKhohang.GUI
@@ -21,7 +23,21 @@ namespace QuanlyKhohang.GUI
         }
         private void Khachhang_Load(object sender, EventArgs e)
         {
-        
+            dataGridView1.Columns.Clear();
+            ConnectString cnn = new ConnectString();
+            string con = cnn.getConnectionString(0);
+            DataSet data = new DataSet();
+
+            using (SqlConnection connect = new SqlConnection(con))
+            {
+                string query = " select Khachhang.IDKH as 'Mã khách hàng', Khachhang.TenKH as 'Họ tên' , Khachhang.Dienthoai as 'SĐT' " 
+ + ", Khachhang.Email as 'Email', Khachhang.Diachi as 'Địa chỉ' from Khachhang ";
+                connect.Open();
+                SqlDataAdapter apter = new SqlDataAdapter(query, connect);
+                apter.Fill(data);
+                connect.Close();
+            }
+            dataGridView1.DataSource = data.Tables[0];
         }
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
